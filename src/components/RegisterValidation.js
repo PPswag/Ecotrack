2e6afe8804fb937a156dafe030dde1888dbfbec3
PPswag <76276487+PPswag@users.noreply.github.com>
2023-06-15 16:50:18 -0700

    Stinky commit

--- src/components/RegisterValidation.js
+++ src/components/RegisterValidation.js
@@ -1,7 +1,32 @@
 import React from 'react'
 
 export default function validation(values) {
-  let errors = {}
-  const email_pattern = /^[^\s@] + @[^\s@] + \.[^\s@] + $/
-  const password_pattern = /^(?=.*\d)(?=.*[A-Z])(a-zA-Z0-9){8,}$/
+  let error = {}
+  const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+  const password_pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+
+  if (values.name === "") {
+    error.name = "Name shouldn't be empty"; 
+  } else {
+    error.name = ""; 
+  }
+
+  if (values.email === "") {
+    error.email = "Email shouldn't be empty"
+  } else if (!email_pattern.test(values.email)) {
+    error.email = "Email didn't match"
+  } else {
+    error.email = ""
+  }
+
+  if (values.password === "") {
+    error.password = "Password shouldn't be empty"
+  } else if (!password_pattern.test(values.password)) {
+    error.password = "Password didn't match"
+  } else {
+    error.password = ""
+  }
+
+  return error;
 }
